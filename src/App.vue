8f2d3edd38d23ca7ce9e8b32423616429039a1b2
Anil rana <anilrana01.gh@gmail.com>
2022-03-27 16:52:52 +0530

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
-<script setup lang="ts">
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+export default{
+  components:{
+    Header,
+    Footer,
+  }
+}
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Header/>
+
+  <img src="./assets/coming-soon-blog.png" class="imgCom" alt="">
+  
+  <Footer />
 </template>
 
 <style>
@@ -16,6 +27,10 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+.imgCom{
+  width: 100%;
 }
 </style>
---
+++ src/components/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script>
+export default {
+    name: Index
+}
+</script>
+
+<style>
+
+</style>
